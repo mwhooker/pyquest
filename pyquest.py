4fd73f7ad5e66cf7505494af091f8e6c5dfa55a9
Matthew Hooker <mwhooker@gmail.com>
2011-12-29 06:33:34 -0500

    can move user around screen.

--- pyquest.py
+++ pyquest.py
@@ -4,20 +4,89 @@ import sys
 import time
 
 
-DIRECTIONS = (curses.KEY_DOWN, curses.KEY_UP, curses.KEY_LEFT, curses.KEY_RIGHT)
 
-
-class Character(object):
+class Spawn(object):
+    """MOB & Users"""
 
     def __init__(self, avatar, y, x):
         self.avatar = avatar
         self.y = y
         self.x = x
-        self.obstructing = None
+        self.zone = None
+
+    def set_zone(self, zone):
+        self.zone = zone
+
+    def move_to(self, y, x):
+        self.zone.unset_field(self.y, self.x)
+        self.y = y
+        self.x = x
+        self.zone.set_field(y, x, self)
 
 
-    def move_cardinal(self, key, screen, units=1):
-        y, x = old_y, old_x = self.y, self.x
+class Zone(object):
+    """Keeps track of what's on the field. Does collision detection, etc."""
+
+    def __init__(self, y, x, screen):
+        self.y = y
+        self.x = x
+        self.screen = screen
+
+        self.field = []
+        for i in xrange(y):
+            row = []
+            for j in xrange(x):
+                row.append(None)
+            self.field.append(row)
+
+        self.spawns = {}
+
+    def add_spawn(self, spawn):
+        """Should immediately render spawn on map."""
+        spawn.set_zone(self)
+        self.set_field(spawn.y, spawn.x, spawn)
+
+    def unset_field(self, y, x):
+        self.field[y][x] = None
+        self.screen.update(y, x, ' ')
+
+    def set_field(self, y, x, obj):
+        self.field[y][x] = obj
+        #self.spawns[spawn] = (spawn.y, spawn.x)
+        self.screen.update(y, x, obj.avatar)
+
+
+
+class Screen(object):
+    """Abstraction to curses."""
+
+    def __init__(self, window):
+        self.window = window
+
+    def update(self, y, x, ch):
+        self.window.addch(y, x, ch)
+
+
+    """
+    def mob_move(self, spawn, old_y, old_x):
+        self.window.addch(old_y, old_x)
+        self.window.addch(spawn.y, spawn.x, spawn.avatar)
+    """
+
+
+class UserControl(object):
+    """Moves the character around the zone"""
+
+    def __init__(self, mob):
+        self.mob = mob
+
+    def accept(self, ch):
+        if ch in (curses.KEY_DOWN, curses.KEY_UP,
+                  curses.KEY_LEFT, curses.KEY_RIGHT):
+            self.move_cardinal(ch)
+
+    def move_cardinal(self, key):
+        y, x = old_y, old_x = self.mob.y, self.mob.x
         if key == curses.KEY_DOWN:
             y += 1
         elif key == curses.KEY_UP:
@@ -28,45 +97,29 @@ class Character(object):
             x += 1
         else:
             return
-        self.y, self.x = y, x
-        self.obstructing = screen.mob_move(self, old_y, old_x, self.obstructing)
+        self.mob.move_to(y, x)
 
-
-class Zone(object):
-
-    def __init__(self):
-        pass
-
-class Screen(object):
-
-    def __init__(self, window):
-        self.window = window
-
-    def mob_move(self, spawn, old_y, old_x, obstructing):
-        will_obstruct = self.window.getch(spawn.y, spawn.x) 
-        """
-        if obstructing:
-            self.window.addch(old_y, old_x, obstructing)
-        """
-        self.window.addch(spawn.y, spawn.x, spawn.avatar)
-        return will_obstruct
 
 
 def main(window):
     curses.curs_set(0)
+    curses.cbreak()
     window.nodelay(1)
     window.border(0)
 
     screen = Screen(window)
+    zone = Zone(100, 100, screen)
+    user = Spawn('@', 1, 1)
+    control = UserControl(user)
 
-    user = Character('@', 1, 1)
+    zone.add_spawn(user)
 
     while True:
         ch = window.getch()
-        if ch in DIRECTIONS:
-            user.move_cardinal(ch, screen)
+        control.accept(ch)
+        window.refresh()
 
-            
+
 
 try:
     curses.wrapper(main)
