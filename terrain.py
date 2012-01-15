7a05d4714ced3e125fc19f881e7280d4d77b1fea
Matthew Hooker <mwhooker@gmail.com>
2012-01-15 03:58:47 -0500

    get terrain ready for mapping to zone.

--- terrain.py
+++ terrain.py
@@ -1,25 +1,8 @@
 from __future__ import division
 from noise import Perlin, noise
 
-def generate(y, x):
-    """Generate terrain gradient of dimensions y, x."""
 
-    p = Perlin()
-    rows = []
-
-    for y1 in xrange(y):
-        cols = []
-        for x1 in xrange(x):
-            cols.append(
-                p.noise(y1 * ( 1 / y), x1 * (1 / x))
-            )
-        rows.append(cols)
-    return rows
-
-
-def generate2(y, x, noise_f, filters=None):
-    if not filters:
-        filters = []
+def generate(y, x, noise_f):
     incr = 0.02
     yoff = 0.0
     rows = []
@@ -29,18 +12,17 @@ def generate2(y, x, noise_f, filters=None):
         cols = []
         for x1 in xrange(x):
             xoff += incr
-            cell = noise_f(yoff, xoff)
-            for f in filters:
-                assert callable(f)
-                cell = f(cell)
-            #cols.append(cell)
-            #cols.append((cell * 127) + 127)
-            if cell > 0.25:
-                cols.append(255)
-            else:
-                cols.append(0)
+            cols.append(noise_f(yoff, xoff))
         rows.append(cols)
     return rows
+
+def mountains(cell):
+    if cell > 0.25:
+        return 255
+    return 0
+
+def clouds(cell):
+    return (cell * 127) + 127
 
 
 if __name__ == '__main__':
@@ -53,17 +35,15 @@ if __name__ == '__main__':
     f = Bitmap('output.bmp', size, size)
 
     perlin = Perlin()
-    p = noise(6)
-    p2 = lambda x, y: perlin.noise(x, y)
-    filters = [
-        lambda cell: (cell * 127) + 127,
-        #lambda cell: 255 if cell > 150 else 0
-    ]
-    n= generate2(size, size, p)
+    pnoise_harmonic = noise(6)
+    pnoise = perlin.noise
+
+    n = generate(size, size, pnoise_harmonic)
 
     for y, row in enumerate(n):
         for x, col in enumerate(row):
-            f.set_pixel(y, x, rgb(col))
+            val = mountains(col)
+            f.set_pixel(y, x, rgb(val))
 
     f.flush()
     f.close()
